229017e63fec6e8524b76d1151b70adabc218bb5
Big D <danialtajalli@outlook.com>
2026-06-07 14:02:16 +0330

    [D] Adding product route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('product/{company}/{car}/{model}/{part}', [ProductController::class, 'show'])
+    ->name('product.show');
