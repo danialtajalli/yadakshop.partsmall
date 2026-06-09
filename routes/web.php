7ca2f3abe717ffe2122f15a5f4578ec2ab99b17b
Big D <danialtajalli@outlook.com>
2026-06-09 11:35:03 +0330

    [D] Adding route for car parts

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,15 @@
 <?php
 
+use App\Http\Controllers\PartSelectionController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('car/{company}/{car}/{model}', [PartSelectionController::class, 'show'])
+    ->name('car.parts');
 
 Route::get('product/{company}/{car}/{model}/{part}', [ProductController::class, 'show'])
     ->name('product.show');
