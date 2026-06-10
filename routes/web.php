0f5127e95b5eff1a4bc3697480aa186697d91a44
Big D <danialtajalli@outlook.com>
2026-06-10 12:44:24 +0330

    [D] Adding representation routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\PartSelectionController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\RepresentationController;
 use App\Http\Controllers\RepairShopController;
 use App\Http\Controllers\ShopController;
 use Illuminate\Support\Facades\Route;
@@ -14,6 +15,8 @@ Route::get('shops', [ShopController::class, 'index'])->name('shops.index');
 Route::get('profile/{shop_slug}', [ShopController::class, 'show'])->name('shop.profile');
 Route::get('repair_shops', [RepairShopController::class, 'index'])->name('repair-shops.index');
 Route::get('repair_profile/{repair_shop_slug}', [RepairShopController::class, 'show'])->name('repair-shop.profile');
+Route::get('representations', [RepresentationController::class, 'index'])->name('representations.index');
+Route::get('representation/{representation_slug}', [RepresentationController::class, 'show'])->name('representation.profile');
 
 Route::get('car/{company}/{car}/{model}', [PartSelectionController::class, 'show'])
     ->name('car.parts');
