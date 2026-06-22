56b63fc0866c1d048c7ee6187a37c271df50fe03
Big D <danialtajalli@outlook.com>
2026-06-22 14:09:01 +0330

    [D] Making vite usable in network

--- vite.config.js
+++ vite.config.js
@@ -17,6 +17,18 @@ export default defineConfig({
         tailwindcss(),
     ],
     server: {
+        host: '0.0.0.0',
+        port: 5173,
+
+        cors: {
+            origin: [
+                'http://192.168.9.72:8000',
+            ],
+        },
+
+        hmr: {
+            host: '192.168.9.72',
+        },
         watch: {
             ignored: ['**/storage/framework/views/**'],
         },
