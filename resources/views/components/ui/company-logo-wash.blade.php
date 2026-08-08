89124c5bcd7a71af437a72935b4b475fd2b279bf
Big D <danialtajalli@outlook.com>
2026-08-08 16:56:24 +0330

    [H] Fixing company logo position in product select path

--- resources/views/components/ui/company-logo-wash.blade.php
+++ resources/views/components/ui/company-logo-wash.blade.php
@@ -4,16 +4,16 @@
 
 @if (filled($logoUrl))
     <div
-        {{ $attributes->merge(['class' => 'pointer-events-none absolute inset-y-0 end-0 w-[55%] max-w-md sm:w-[45%]']) }}
+        {{ $attributes->merge(['class' => 'pointer-events-none absolute inset-y-0 end-0 w-[48%] max-w-sm overflow-hidden sm:w-[40%]']) }}
         aria-hidden="true"
     >
         <img
             src="{{ $logoUrl }}"
             alt=""
-            class="absolute end-[-10%] top-1/2 h-[140%] w-auto max-w-none -translate-y-1/2 object-contain opacity-[0.12] sm:opacity-[0.14]"
+            class="absolute inset-0 size-full object-contain object-center p-3 opacity-[0.12] sm:p-5 sm:opacity-[0.14]"
             loading="lazy"
             decoding="async"
         >
-        <div class="absolute inset-0 bg-gradient-to-r from-transparent via-white/40 to-white"></div>
+        <div class="absolute inset-0 bg-gradient-to-r from-transparent via-white/35 to-white"></div>
     </div>
 @endif
